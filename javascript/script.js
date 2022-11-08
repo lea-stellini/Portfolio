e5b865dc921ba817117384d0fee4c07d51c13ed9
lea-stellini <lea.stellini@gmail.com>
2022-11-08 15:52:11 +0100

    added dark mode

--- javascript/script.js
+++ javascript/script.js
@@ -1,59 +1,31 @@
-const toggle = document.getElementById('toggle');
+class CssPropControl {
+    constructor(element){
+        this.element = element
+    }
+    get(varName){
+        return getComputedStyle(this.element).getPropertyValue(varName)
+    }
+    set(varName, val){
+        return this.element.style.setProperty(varName, val)
+    }
+}
 
+const bodyCssProps = new CssPropControl(document.body);
+
+let toggle = document.querySelector('#dark-mode-toggle');
 
 
 toggle.addEventListener('click', () => {
-   inEnglish();
-   inFrench();
-    })
+   let mode = toggle.checked ? 'dark' :'light'
+   bodyCssProps.set('--background', bodyCssProps.get(`--${mode}-background`))
+   bodyCssProps.set('--primary', bodyCssProps.get(`--${mode}-primary`))
+   bodyCssProps.set('--secondary', bodyCssProps.get(`--${mode}-secondary`))
+   bodyCssProps.set('--tertiary', bodyCssProps.get(`--${mode}-tertiary`))
+   bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`))
+   bodyCssProps.set('--header', bodyCssProps.get(`--${mode}-header`))
+})
 
 
-const inEnglish = (sentence) => {
-    const french = document.querySelectorAll(".french");
-    for(let i=0; i < french.length; i++){
-        french[i].classList.toggle("done");
-    }
-
-    localStorageUpdate();
-}
-
-const inFrench = (sentence) => {
-    const english = document.querySelectorAll(".english");
-    for(let i=0; i < english.length; i++){
-        english[i].classList.toggle("done");
-    }
-
-    localStorageUpdate();
-}
-
-const localStorageUpdate = () => {
-    const savesFrench = document.querySelectorAll('.french');
-    const savesEnglish = document.querySelectorAll('.english');
-
-    const langue = [];
-
-    savesFrench.forEach((save) => {
-        langue.push({
-            done: save.classList.contains('done'),
-        });
-    });
-
-    savesEnglish.forEach((save) => {
-        langue.push({
-            done: save.classList.contains('done'),
-        });
-    });
-
-    localStorage.setItem("langue", JSON.stringify(langue));
-}
-
-let langue = localStorage.getItem("langue");
-let langueJson = JSON.parse(langue);
 
 
-if(langueJson){
-    langueJson.forEach(sentence => {
-        sentence = langue.done;
-    })
-        
-    }
+
