1bc5d414780f5ddb397b0ab1f5ab2a25d06929c4
lea-stellini <lea.stellini@gmail.com>
2023-10-16 11:36:30 +0200

    pb imd deploy

--- src/layouts/Skills/Skills.tsx
+++ src/layouts/Skills/Skills.tsx
@@ -2,6 +2,7 @@ import Divider from '../../components/Divider/Divider';
 import Button from '../../components/Button/Button';
 import cv from '../../assets/CV_STELLINI_LEA.pdf';
 import './Skills.css';
+import skill from '../../assets/skills/html5.svg'
 
 function Skills(){
 
@@ -18,29 +19,29 @@ function Skills(){
                     <div className='skill'>
                         <h3>Front-End</h3>
                         <div className='skill__grid'>
-                            <img src="../../../images/skills/html5.svg" alt="html" width="40" height="40"/>
-                            <img src="../../../images/skills/react.svg" alt="react" width="40" height="40"/>
-                            <img src="../../../images/skills/vuejs.svg" alt="vuejs" width="40" height="40"/>
-                            <img src="../../../images/skills/js.svg" alt="javascript" width="40" height="40"/>
-                            <img src="../../../images/skills/css3-alt.svg" alt="css" width="40" height="40"/>
-                            <img src="../../../images/skills/sass.svg" alt="sass" width="40" height="40"/>
-                            <img src="../../../images/skills/bootstrap.svg" alt="Bootstrap" width="40" height="40"/>
+                            <img src="../../assets/skills/html5.svg" alt="html" width="40" height="40"/>
+                            <img src="../../assets/skills/react.svg" alt="react" width="40" height="40"/>
+                            <img src="../../assets/skills/vuejs.svg" alt="vuejs" width="40" height="40"/>
+                            <img src="../../assets/skills/js.svg" alt="javascript" width="40" height="40"/>
+                            <img src="../../assets/skills/css3-alt.svg" alt="css" width="40" height="40"/>
+                            <img src="../../assets/skills/sass.svg" alt="sass" width="40" height="40"/>
+                            <img src="../../assets/skills/bootstrap.svg" alt="Bootstrap" width="40" height="40"/>
                         </div>
                     </div>
                     <div className='skill'>
                         <h3>Back-End</h3>
                         <div className='skill__grid'>
-                            <img src="../../../images/skills/node.svg" alt="Node" width="40" height="40"/>
-                            <img src="../../../images/skills/Expressjs.webp" alt="Express" width="40" height="40"/>
-                            <img src="../../../images/skills/MongoDB.webp" alt="MongoDB" width="40" height="40"/>
+                            <img src="../../assets/skills/node.svg" alt="Node" width="40" height="40"/>
+                            <img src="../../assets/skills/Expressjs.webp" alt="Express" width="40" height="40"/>
+                            <img src="../../assets/skills/MongoDB.webp" alt="MongoDB" width="40" height="40"/>
                         </div>
                     </div>
                     <div className='skill'>
                         <h3>Outils</h3>
                         <div className='skill__grid'>
-                            <img src="../../../images/skills/figma.svg" alt="figma" width="40" height="40"/>
-                            <img src="../../../images/skills/git.svg" alt="git" width="40" height="40"/>
-                            <img src="../../../images/skills/gitlab.svg" alt="gitlab" width="40" height="40"/>
+                            <img src="../../assets/skills/figma.svg" alt="figma" width="40" height="40"/>
+                            <img src="../../assets/skills/git.svg" alt="git" width="40" height="40"/>
+                            <img src="../../assets/skills/gitlab.svg" alt="gitlab" width="40" height="40"/>
                         </div>
                     </div>
                 </div>
