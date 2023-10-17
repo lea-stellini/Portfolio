b37c86342f26e6b1301aecf90f03ce74c558597c
lea-stellini <lea.stellini@gmail.com>
2023-10-17 10:21:59 +0200

    update modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -8,31 +8,29 @@ import './Modal.css';
 type Props = {
     isOpen: boolean;
     isClose: (e : React.MouseEvent) => void;
+    onClick?: (e : React.MouseEvent) => void;
 }
 
-function ProjectModal({isOpen, isClose}: Props) {
+function ProjectModal({isOpen, isClose, onClick}: Props) {
     return(
         <Modal isOpen={isOpen} className="modal__container">
             <div className='modal__header'>
-                <h2>Projet</h2>
-                <button onClick={isClose}>X</button>
+                <h2>Bookie</h2>
+                <button className='btn__close' onClick={isClose}>X</button>
             </div>
             <div className='modal__body'>
                 <div className='modal__description'>
                     
-                    <h3>Technologies</h3>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                    </ul>
-                    <h3>Descriptif</h3>
+                    <h4>Technologies</h4>
+                    <p>HTML | CSS</p>
+                    <h4>Descriptif</h4>
                     <p>Participation à la conception du site portfolio d’une architect d’intérieur.
                         Pour ce projet, mes missions ont été de : 
                         Développer la page de présentation des travaux de l’architecte (galerie avec filtres). 
                         Développer la page de connexion à l’espace administrateur.
                         Développer une modale permettant d’ajouter de nouveaux projets avec une photo.
                     </p>
-                    <h3>Compétences</h3>
+                    <h4>Compétences</h4>
                     <ul>
                         <li>Manipuler les éléments du DOM avec JavaScript</li>
                         <li>Récupérer les données utilisateurs dans le JavaScript via des formulaires
@@ -40,12 +38,11 @@ function ProjectModal({isOpen, isClose}: Props) {
                         <li>Gérer les événements utilisateurs avec JavaScript
                         </li>
                     </ul>
-                    <Button title="voir le site" />
+                    <Button title="voir le site" onClick={onClick}/>
                 </div>
                 <div className='modal__images'>
-                    <img src={cover} alt="" />
-                    <img src={img1} alt="" />
                     <img src={img2} alt="" />
+                    <img src={img1} alt="" />
                 </div>
             </div>
             
