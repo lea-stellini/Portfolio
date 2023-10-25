5278006d79688e11e256d7c561309c36fd1146b0
lea-stellini <lea.stellini@gmail.com>
2023-10-25 14:53:49 +0200

    test

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -20,17 +20,19 @@ function Header() {
         }
     }
 
+    
+
  return(
     <div className="header__container">
         <div className="header">
-            <Link to="/#banner"><img  className="header__img" src={logo} alt="logo contenant le prénom Léa" width="40" height="40"/></Link>
+            <Link to="/Portfolio-pro/#banner"><img  className="header__img" src={logo} alt="logo contenant le prénom Léa" width="40" height="40"/></Link>
             <nav className={`header__nav ${hamburgerOpen ? "nav__hamburger" : "none" }`}>
                 <ul>
-                    <li><Link to="/#about" onClick={mobileDevice}>A PROPOS</Link></li>
-                    <li><Link to="/#projects" onClick={mobileDevice}>PROJETS</Link></li>
-                    <li><Link to="/#skills" onClick={mobileDevice}>COMPETENCES</Link></li>
-                    <li><Link to="/#path" onClick={mobileDevice}>PARCOURS</Link></li>
-                    <li><Link to="/#contact"><Button title="contact" onClick={mobileDevice} /></Link></li>
+                    <li><Link to="#about" onClick={mobileDevice}>A PROPOS</Link></li>
+                    <li><Link to="projects" onClick={mobileDevice}>PROJETS</Link></li>
+                    <li><Link to="skills" onClick={mobileDevice}>COMPETENCES</Link></li>
+                    <li><Link to="path" onClick={mobileDevice}>PARCOURS</Link></li>
+                    <li><Link to="contact"><Button title="contact" onClick={mobileDevice} /></Link></li>
                 </ul>
             </nav>
             <div className="hamburger" onClick={toggleHamburger}>
--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,19 @@ import App from './pages/App/App'
 import Error from './pages/Error/Error'
 import Project from './pages/Project/Project'
 import './index.css'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<App />}></Route>
-          <Route path="/project/:id" element={<Project />}></Route>
+          <Route path="/Portfolio-pro/" element={<App />}></Route>
+          <Route path="/Portfolio-pro/project/:id" element={<Project />}></Route>
           <Route path="*" element={<Error />}></Route>
         </Routes>
         <Footer />
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
 )
