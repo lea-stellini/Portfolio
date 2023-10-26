b7fdbc8f89818c5e787f91361a0a3dc13e3e3681
lea-stellini <lea.stellini@gmail.com>
2023-10-26 15:08:30 +0200

    update _variables.css

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -12,7 +12,7 @@ function Card({title, year, where, skills, inverted} : Props) {
     return(
         <div className={`${inverted ? "card__container__inverted" : "card__container"}`}>
             <div className="card" >
-                <h4>{title}</h4>
+                <h3>{title}</h3>
                 <p>{where}</p>
                 <p>{year}</p>
                 <ul className='card__list'>
