b91bda266b040c50425c2946eda2486259ae7ebd
lea-stellini <lea.stellini@gmail.com>
2023-10-27 14:30:40 +0200

    text about

--- src/layouts/About/About.tsx
+++ src/layouts/About/About.tsx
@@ -9,7 +9,7 @@ function About(){
             <div className='about'>
                 <h2>A Propos</h2>
                 <p className='about__text'>
-                Après avoir découvert l’univers du web par le biais du site internet de mon ancien lieu de travail, curieuse et désireuse d’en apprendre davantage, j’ai commencé par développer mes compétences en autodidacte. <br></br>
+                Après avoir découvert l’univers du web par le biais du site internet de mon ancien lieu de travail, étant curieuse et désireuse d’en apprendre davantage, j’ai commencé par développer mes compétences en autodidacte. <br></br>
                 <br></br>
                 Fin 2022, j’ai débuté une formation d'un an en alternance avec OpenClassroom.<br></br>
                 <br></br>
