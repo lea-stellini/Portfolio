2802b64e317a0830112bb8bc8da0140432bcc44f
lea-stellini <lea.stellini@gmail.com>
2023-10-27 14:30:15 +0200

    text about

--- src/layouts/About/About.tsx
+++ src/layouts/About/About.tsx
@@ -9,9 +9,11 @@ function About(){
             <div className='about'>
                 <h2>A Propos</h2>
                 <p className='about__text'>
-                    Bienvenue! Je m&apos;appelle Léa Stellini et je suis passionnée par l&apos;univers du web.
-                    J&apos;ai commencé à apprendre à coder en 2020 pendant le confinement et depuis je n&apos;ai pas arrêté.
-                    En avril 2022, j&apos;ai décidé de me lancer pleinement dans cette aventure afin de devenir développeuse front-end.
+                Après avoir découvert l’univers du web par le biais du site internet de mon ancien lieu de travail, curieuse et désireuse d’en apprendre davantage, j’ai commencé par développer mes compétences en autodidacte. <br></br>
+                <br></br>
+                Fin 2022, j’ai débuté une formation d'un an en alternance avec OpenClassroom.<br></br>
+                <br></br>
+                Aujourd’hui, je suis ravie de pouvoir partager avec vous les différents projets réalisés lors de celle-ci ! 
                 </p>
             </div>      
         </div>
